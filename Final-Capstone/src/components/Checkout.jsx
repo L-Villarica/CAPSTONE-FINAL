feb3a4a30aab5cd04f26db0913d3976ed53648d2
Lance Villarica <lanceoliver_villarica@yahoo.com>
2023-09-25 22:27:14 -0700

    added more media queries

--- Final-Capstone/src/components/Checkout.jsx
+++ Final-Capstone/src/components/Checkout.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
 export default function CheckoutPage({ cartItems, removeFromCart }) {
   return (
       <div className="checkout-form-container">
